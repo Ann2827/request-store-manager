f949874cbeb83700ff3c179826142144f4bb0983
Bystrova Ann <ann.bystrova96@mail.ru>
2026-04-18 19:07:38 +0300

    feat: add parse original error

--- src/modules/Messages.ts
+++ src/modules/Messages.ts
@@ -2,25 +2,29 @@ import type { IMessagesConfig, IModule, TNotificationsBase, TNotificationType } 
 
 class Messages<N extends TNotificationsBase = TNotificationsBase> implements IModule {
   readonly #codes: IMessagesConfig<N>['codes'];
+  readonly #getOriginalError: IMessagesConfig<N>['getOriginalError'];
 
   constructor(config?: IMessagesConfig<N>) {
     this.#codes = config?.codes;
+    this.#getOriginalError = config?.getOriginalError;
   }
 
   public restart() {}
 
-  public parse(response: Response): [N, TNotificationType] | undefined {
+  public parse(response: Response, data?: unknown): [N, TNotificationType] | undefined {
     if (!this.#codes) return;
+
+    const code = this.#getOriginalError?.(response, data) ?? response.status;
 
     const { default: errDefaultObj, ...codes } = this.#codes;
     const errCodeArr = Object.entries(codes).find(
       ([key]) =>
-        (typeof key === 'number' && key === response.status) ||
-        (typeof key === 'string' && key.split(';').some((i) => Number(i) === response.status)),
+        (typeof key === 'number' && key == code) || (typeof key === 'string' && key.split(';').some((i) => i == code)),
     );
 
-    if (errCodeArr) return [{ ...errDefaultObj, ...errCodeArr[1] }, response.ok ? 'success' : 'error'];
-    return response.ok ? undefined : [{ ...errDefaultObj }, 'error'];
+    const isOk = code.toString()[0] === '2';
+    if (errCodeArr) return [{ ...errDefaultObj, ...errCodeArr[1] }, isOk ? 'success' : 'error'];
+    return isOk ? undefined : [{ ...errDefaultObj }, 'error'];
   }
 }
 
--- src/types/modules/messages.types.ts
+++ src/types/modules/messages.types.ts
@@ -2,4 +2,5 @@ import type { TNotificationsBase } from './notifications.types';
 
 export interface IMessagesConfig<N extends TNotificationsBase> {
   codes?: Record<string | number, N>;
+  getOriginalError?: (response: Response, data: unknown) => string | number | undefined;
 }
